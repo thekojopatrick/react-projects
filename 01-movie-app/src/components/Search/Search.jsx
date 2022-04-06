59b5cdd1f5e7f9e1a38c8712867516e7052598f3
Kojo Patrick <patrickasare84@gmail.com>
2022-04-06 17:49:09 +0000

    add search input

--- 01-movie-app/src/components/Search/Search.jsx
+++ 01-movie-app/src/components/Search/Search.jsx
@@ -1,9 +1,45 @@
+import { FiSearch } from "react-icons/fi";
+import styled from "styled-components";
+
 function Search() {
   return (
-    <form>
+    <SearchForm>
+      <FiSearch />
       <input type="text" placeholder="Search Movies or TV Shows" />
-    </form>
+    </SearchForm>
   );
 }
+
+const SearchForm = styled.form`
+  display: inline-flex;
+  align-items: center;
+  gap: 1rem;
+  overflow: hidden;
+  padding: 1rem;
+  border: solid 1px #475069;
+  border-radius: .9rem;
+  color: #475069;
+
+  input {
+    width: 100%;
+    min-width: 272px;
+    background: transparent;
+    font-size: 0.9rem;
+    line-height: 1.2rem;
+    border: none;
+    outline: none;
+    color: #fff;
+    padding: 0.5rem 0rem;
+    margin-right: 0.5rem;
+
+    &::placeholder{
+      color:#475069;
+    }
+  }
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default Search;
