4ed98ee99a85c582493e3412bdf16e436f19bfa3
Kojo Patrick <patrickasare84@gmail.com>
2022-04-26 13:35:36 +0000

    add button class outline

--- 02-grocery-delivery-website/src/components/button/button.component.jsx
+++ 02-grocery-delivery-website/src/components/button/button.component.jsx
@@ -1,77 +1,75 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Button = ({ children, type }) => {
-  return (
-    <ButtonBox
-      className={
-        type === "btn-primary"
-          ? "btn-primary"
-          : type === "btn-secondary" && "btn-secondary"
-      }
-    >
-      {children}
-    </ButtonBox>
-  );
+export const Button = ({ children, type, className }) => {
+	return (
+		<ButtonBox
+			className={className ? className : type === "btn-primary" ? `btn-primary` : type === "btn-secondary" && "btn-secondary"}
+		>
+			{children}
+		</ButtonBox>
+	);
 };
 const ButtonBox = styled.button`
-  display: flex;
-  flex-direction: row;
-  gap: 0.5rem;
-  align-items: center;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 20px;
-  letter-spacing: -0.01em;
-  color: #363636;
-  text-align: center;
-  padding: 1.2rem 1.8rem;
+	display: flex;
+	flex-direction: row;
+	gap: 0.5rem;
+	align-items: center;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 20px;
+	letter-spacing: -0.01em;
+	color: #363636;
+	text-align: center;
+	padding: 1.2rem 1.8rem;
+	border-radius: 12px;
+	&:hover {
+		color: #f86061;
 
-  &:hover {
-    color: #f86061;
+		svg {
+			box-shadow: 0px 2px 50px -1px rgba(0, 0, 0, 0.4);
+		}
+	}
 
-    svg {
-      box-shadow: 0px 2px 50px -1px rgba(0, 0, 0, 0.4);
-    }
-  }
+	&.btn-primary {
+		color: #fffafa;
+		background: #f86061;
 
-  &.btn-primary {
-    color: #fffafa;
-    background: #f86061;
-    border-radius: 12px;
+		&:hover {
+			color: #fffafa;
+			background: #e13b3b;
+		}
+	}
 
-    &:hover {
-      color: #fffafa;
-      background: #e13b3b;
-    }
-  }
+	&.btn-outline {
+		border: 2px solid #eedcdc;
+		color: #2e2e2e;
 
-  &.btn-outline {
-    border: 1.5px solid #eedcdc;
-    box-sizing: border-box;
-    border-radius: 12px;
-  }
+		&:hover {
+			color: #fffafa;
+			background: #2e2e2e;
+		}
+	}
 
-  svg {
-    display: grid;
-    place-content: center;
-    width: 4.375rem;
-    height: 4.375rem;
-    border-radius: 50vh;
-    padding: 1.6rem;
-    fill: #f86061;
-    background: #ffffff;
-    box-shadow: 0px 20px 49px -10px rgba(0, 0, 0, 0.08);
-  }
+	svg {
+		display: grid;
+		place-content: center;
+		width: 4.375rem;
+		height: 4.375rem;
+		border-radius: 50vh;
+		padding: 1.6rem;
+		fill: #f86061;
+		background: #ffffff;
+		box-shadow: 0px 20px 49px -10px rgba(0, 0, 0, 0.08);
+	}
 
-  &.btn-secondary {
-    color: #f86061;
-    border: 1.5px solid #f86061;
-    border-radius: 12px;
+	&.btn-secondary {
+		color: #f86061;
+		border: 1.5px solid #f86061;
 
-    &:hover {
-      color: #fffafa;
-      background: #e13b3b;
-    }
-  }
+		&:hover {
+			color: #fffafa;
+			background: #e13b3b;
+		}
+	}
 `;
--- 02-grocery-delivery-website/src/components/cta/cta.component.jsx
+++ 02-grocery-delivery-website/src/components/cta/cta.component.jsx
@@ -8,34 +8,31 @@ import { Button } from "../components";
 
 const CTA = () => {
   return (
-    <section className="cta">
-      <div className="cta-content">
-        <div className="col">
-          <img src={MobileMockup} alt="mobile-mockup" />
-        </div>
-        <div className="col">
-          <div className="col-content">
-            <h3>Download our app</h3>
-            <h2>Get the Groceries app order more easily.</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut.
-            </p>
-            <div className="btn-group">
-              <Button type={"btn-outline"}>
-                <img src={AppStoreIcon} alt="app" />
-                App Store
-              </Button>
-              <Button>
-                <img src={PlayStoreIcon} alt="app" />
-                Play Store
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+		<section className="cta">
+			<div className="cta-content">
+				<div className="col">
+					<img src={MobileMockup} alt="mobile-mockup" />
+				</div>
+				<div className="col">
+					<div className="col-content">
+						<h3>Download our app</h3>
+						<h2>Get the Groceries app order more easily.</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+						<div className="btn-group">
+							<Button className={"btn-outline"}>
+								<img src={AppStoreIcon} alt="app" />
+								App Store
+							</Button>
+							<Button className={"btn-outline"}>
+								<img src={PlayStoreIcon} alt="app" />
+								Play Store
+							</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default CTA;
