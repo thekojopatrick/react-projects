30d04fb999946ebc9785c9693766e51727ff8eb9
Kojo Patrick <patrickasare84@gmail.com>
2022-04-27 10:42:31 +0000

    clean footer

--- 02-grocery-delivery-website/src/components/footer/footer.component.jsx
+++ 02-grocery-delivery-website/src/components/footer/footer.component.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { Logo } from "../../constants/images";
 import "./footer.styles.scss";
 
@@ -6,44 +7,30 @@ const Footer = () => {
 		<footer className="footer">
 			<nav>
 				<div className="section section-1">
-					<div className="footer-logo">
+					<NavLink to="/" className="footer-logo">
 						<img src={Logo} alt="Grover" />
 						Grover
-					</div>
+					</NavLink>
 					<p>Fast delivery,which is active all over the world,serves with many transportation vehicles.</p>
 				</div>
 				<div className="section section-2">
 					<h4>Company</h4>
 					<ul className="footer-list">
 						<li className="footer-item">
-							<a href="/about">About Us</a>
+							<NavLink to="/about-us">About Us</NavLink>
 						</li>
-						<li className="footer-item">
-							<a href="/blog">Blog</a>
-						</li>
-						<li className="footer-item">
-							<a href="/products">All Products</a>
-						</li>
-						<li className="footer-item">
-							<a href="/office_location">Locations Map</a>
-						</li>
+						<li className="footer-item">Blog</li>
+						<li className="footer-item">All Products</li>
+						<li className="footer-item">Locations Map</li>
 					</ul>
 				</div>
 				<div className="section section-3">
 					<h4>Services</h4>
 					<ul className="footer-list">
-						<li className="footer-item">
-							<a href="/order_tracking">Order tracking</a>
-						</li>
-						<li className="footer-item">
-							<a href="/wishlist">Wish List</a>
-						</li>
-						<li className="footer-item">
-							<a href="/login">My account</a>
-						</li>
-						<li className="footer-item">
-							<a href="/policies">Terms & Conditions</a>
-						</li>
+						<li className="footer-item">Order tracking</li>
+						<li className="footer-item">Wish List</li>
+						<li className="footer-item">My account</li>
+						<li className="footer-item">Terms & Conditions</li>
 					</ul>
 				</div>
 				<div className="section section-4">
@@ -55,12 +42,8 @@ const Footer = () => {
 			<div className="footnote">
 				<div className="copyright">All Rights Resevered &copy; BESINK 2021</div>
 				<ul>
-					<li>
-						<a href="/policies">Terms & Conditions</a>
-					</li>
-					<li>
-						<a href="/policies">Privacy & Policy</a>
-					</li>
+					<li>Terms & Conditions</li>
+					<li>Privacy & Policy</li>
 				</ul>
 			</div>
 		</footer>
